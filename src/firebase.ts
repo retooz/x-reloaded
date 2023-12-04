a8c8bc27a99890dd1f2c7c49c981c76f6dfa69f9
retooz <retoz@knou.ac.kr>
2023-12-05 00:33:48 +0900

    [FEAT] Add Login Function - 초기화 - firebase 세팅 - 로그인 기능 추가 - 회원가입 기능 추가 - github 로고 추가 - github 연동 로그인 기능 추가

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDznN7teVZkdXsnk4Cw9iadYLJNemnhk8Q",
+  authDomain: "x-reloaded-b2f60.firebaseapp.com",
+  projectId: "x-reloaded-b2f60",
+  storageBucket: "x-reloaded-b2f60.appspot.com",
+  messagingSenderId: "282139525968",
+  appId: "1:282139525968:web:8c397e2624c194a3db6255"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
---
+++ src/routes/login.tsx
@@ -0,0 +1,74 @@
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { FirebaseError } from 'firebase/app';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { auth } from '../firebase';
+import { Error, Form, Input, Switcher, Title, Wrapper } from '../components/auth-components';
+import GithubButton from '../components/github-btn';
+
+export default function CreateAccount() {
+  const navigate = useNavigate();
+  const [isLoading, setLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { name, value },
+    } = e;
+    if (name === 'email') {
+      setEmail(value);
+    } else if (name === 'password') {
+      setPassword(value);
+    }
+  };
+  const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError('');
+    if (isLoading || email === '' || password === '') return;
+    try {
+      setLoading(true);
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate('/');
+    } catch (err) {
+      if (err instanceof FirebaseError) {
+        setError(err.message);
+      }
+    } finally {
+      setLoading(false);
+    }
+  };
+  return (
+    <Wrapper>
+      <Title>Log Into 𝕏</Title>
+      <Form onSubmit={onSubmit}>
+        <Input
+          onChange={onChange}
+          name='email'
+          value={email}
+          placeholder='Email'
+          type='email'
+          required
+        />
+        <Input
+          onChange={onChange}
+          name='password'
+          value={password}
+          placeholder='Password'
+          type='password'
+          required
+        />
+        <Input
+          type='submit'
+          value={isLoading ? 'Loading...' : 'Log In'}
+        />
+      </Form>
+      {error !== '' ? <Error>{error}</Error> : null}
+      <Switcher>
+        계정이 없으세요? {' '}
+        <Link to='/create-account'>계정 생성하기 &rarr;</Link>
+      </Switcher>
+      <GithubButton/>
+    </Wrapper>
+  );
+}
