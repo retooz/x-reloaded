02f1fa8e9eb0307b908da8cd5a63efffa14b4133
retooz <retoz@knou.ac.kr>
2023-12-08 22:51:40 +0900

    [FEAT] Add post function - 트윗 작성 기능 추가

--- src/components/post-tweet-form.tsx
+++ src/components/post-tweet-form.tsx
@@ -1,5 +1,7 @@
+import { addDoc, collection } from 'firebase/firestore';
 import { useState } from 'react';
 import styled from 'styled-components';
+import { auth, db } from '../firebase';
 
 const Form = styled.form`
   display: flex;
@@ -75,8 +77,27 @@ export default function PostTweetForm() {
     }
   };
 
+  const onSubmit = async(e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const user = auth.currentUser;
+    if(!user || isLoading || tweet === '' || tweet.length > 180) return;
+    try {
+      setLoading(true);
+      await addDoc(collection(db, 'tweets'), {
+        tweet,
+        createdAt: Date.now(),
+        username: user.displayName || 'Anonymous',
+        userID: user.uid,
+      })
+    } catch(err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   return (
-    <Form>
+    <Form onSubmit={onSubmit}>
       <TextArea
         rows={5}
         maxLength={180}
